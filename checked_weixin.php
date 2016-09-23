e2eedc1ae18dcdbf38da236d74a6023232b56554
jiaominlong <jiaominlong@outlook.com>
2016-09-23 20:00:25 +0800

    封装微信对象

---
+++ checked_weixin.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jml
+ * Date: 2016/9/23
+ * Time: 19:27
+ */
+
+define('TOKEN','weixin');
+
+class weixin{
+
+    public $timestamp;
+    public $nonce;
+    public $signature;
+    public $echostr;
+
+    public function __construct($timestamp, $nonce, $signature, $echostr){
+        $this->timestamp    =   $timestamp;
+        $this->nonce        =   $nonce;
+        $this->signature    =   $signature;
+        $this->echostr      =   $echostr;
+
+    }
+
+    private function checkaccess(){
+        $arr = array(TOKEN, $this->timestamp, $this->nonce);
+        $tmpStr = implode(sort($arr, SORT_STRING));
+        $tmpStr = sha1($tmpStr);
+        if ($tmpStr == $this->signature){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function returnStr(){
+        if ($this->checkaccess()){
+            return $this->echostr;
+        }else{
+            return false;
+        }
+    }
+
+}
--- wei_index.php
+++ wei_index.php
@@ -6,25 +6,39 @@
  * Time: 14:44
  */
 
+//// * 1.将timestamp，nonce,token按字段徐排序
+//    $timestamp  =   $_GET['timestamp'];
+//    $nonce      =   $_GET['nonce'];
+//    $token      =   'weixin';
+//    $signature  =   $_GET['signature'];
+//
+//    $arr        =   array($token,$timestamp,$nonce);
+//    sort($arr, SORT_STRING);
+//
+//// * 2.将排序后的三个参数拼接成字符串后用sha1加密
+//
+//    $tmpStr     =   implode($arr);
+//    $tmpStr     =   sha1($tmpStr);
+//
+//// * 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
+//
+//    if ($signature == $tmpStr){
+//        echo $_GET['echostr'];
+//        exit();
+//    }
 
-// * 1.将timestamp，nonce,token按字段徐排序
-    $timestamp  =   $_GET['timestamp'];
-    $nonce      =   $_GET['nonce'];
-    $token      =   'weixin';
-    $signature  =   $_GET['signature'];
+require_once 'checked_weixin.php';
 
-    $arr        =   array($token,$timestamp,$nonce);
-    sort($arr, SORT_STRING);
-
-// * 2.将排序后的三个参数拼接成字符串后用sha1加密
-
-    $tmpStr     =   implode($arr);
-    $tmpStr     =   sha1($tmpStr);
-
-// * 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
-
-    if ($signature == $tmpStr){
-        echo $_GET['echostr'];
-        exit();
+    if(isset($_GET['signature'])){
+        $timestamp  =   $_GET['timestamp'];
+        $nonce      =   $_GET['nonce'];
+        $signature  =   $_GET['signature'];
+        $echostr    =   $_GET['echostr'];
+        $wx = new weixin($timestamp, $nonce, $signature, $echostr);
+        echo $wx->returnStr();
+    }else{
+        echo 'test';
     }
+
+
 
