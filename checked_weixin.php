aba4bcc1730b59bd790ccdb0a7e27cdabf8b2805
jiaominlong <jiaominlong@outlook.com>
2016-09-23 20:38:22 +0800

    修改文件注释

--- checked_weixin.php
+++ checked_weixin.php
@@ -4,6 +4,9 @@
  * User: jml
  * Date: 2016/9/23
  * Time: 19:27
+ * 1.将timestamp，nonce,token按字段徐排序
+ * 2.将排序后的三个参数拼接成字符串后用sha1加密
+ * 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
  */
 
 define('TOKEN','weixin');
--- wei_index.php
+++ wei_index.php
@@ -5,28 +5,6 @@
  * Date: 2016/9/23
  * Time: 14:44
  */
-
-//// * 1.将timestamp，nonce,token按字段徐排序
-//    $timestamp  =   $_GET['timestamp'];
-//    $nonce      =   $_GET['nonce'];
-//    $token      =   'weixin';
-//    $signature  =   $_GET['signature'];
-//
-//    $arr        =   array($token,$timestamp,$nonce);
-//    sort($arr, SORT_STRING);
-//
-//// * 2.将排序后的三个参数拼接成字符串后用sha1加密
-//
-//    $tmpStr     =   implode($arr);
-//    $tmpStr     =   sha1($tmpStr);
-//
-//// * 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
-//
-//    if ($signature == $tmpStr){
-//        echo $_GET['echostr'];
-//        exit();
-//    }
-
 require_once 'checked_weixin.php';
 
     if(isset($_GET['signature'])){
