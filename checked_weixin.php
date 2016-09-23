a0815002bd2d9439e76dd0e1fbe5902e85a8f445
jiaominlong <jiaominlong@outlook.com>
2016-09-23 21:43:47 +0800

    修改文件注释

--- checked_weixin.php
+++ checked_weixin.php
@@ -11,7 +11,7 @@
 
 define('TOKEN','weixin');
 
-class weixin{
+class initWeichat{
 
     public $timestamp;
     public $nonce;
@@ -25,7 +25,6 @@ class weixin{
         $this->echostr      =   $echostr;
 
     }
-
     private function checkaccess(){
         $arr = array(TOKEN, $this->timestamp, $this->nonce);
         sort($arr, SORT_STRING);
@@ -36,7 +35,6 @@ class weixin{
             return $tmpStr;
         }
     }
-
     public function returnStr(){
         if ($this->checkaccess()){
             return $this->echostr;
@@ -44,5 +42,45 @@ class weixin{
             return false;
         }
     }
-
 }
+
+class messageWeichat{
+    public $postObj;
+//    public $ToUserName;
+//    public $FromUserName;
+//    public $CreateTime;
+//    public $MsgType;
+//    public $Content;
+//    public $MsgId;
+//    public $Event;
+
+    public function __construct($defaultXML){
+        $this->postObj = simplexml_load_string($defaultXML);
+    }
+    // 判断事件
+    public function judge_event(){
+        if (strtolower($this->postObj->MsgType) == 'event'){
+            //如果是关注 subscribe 事件
+            if (strtolower($this->postObj->Event) == 'subscribe'){
+               //回复用户消息
+                $toUser     =   $this->postObj->FromUserName;
+                $fromUser   =   $this->postObj->toUserName;
+                $time       =   time();
+                $Msgtype    =   'text';
+                $Content    =   '欢迎关注常熟服装在线优惠买单';
+
+                $tmplate = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>12345678</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                            </xml>";
+
+                $info   =   sprintf($tmplate,$toUser,$fromUser, $time, $Msgtype, $Content);
+                echo $info;
+            }
+        }
+    }
+}
+
--- wei_index.php
+++ wei_index.php
@@ -7,16 +7,25 @@
  */
 require_once 'checked_weixin.php';
 
+//    init
     if(isset($_GET['signature'])){
         $timestamp  =   $_GET['timestamp'];
         $nonce      =   $_GET['nonce'];
         $signature  =   $_GET['signature'];
         $echostr    =   $_GET['echostr'];
-        $wx = new weixin($timestamp, $nonce, $signature, $echostr);
+        $wx = new initWeichat($timestamp, $nonce, $signature, $echostr);
         echo $wx->returnStr();
+        exit();
     }else{
         echo 'test';
     }
+
+//message
+   if($GLOBALS['HTTP_RAW_POST_DATA']){
+       $message = new messageWeichat($GLOBALS['HTTP_RAW_POST_DATA']);
+   }
+
+
 
 
 
