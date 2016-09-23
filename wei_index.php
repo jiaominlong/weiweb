c39fb3bc1c585582dc9e659f7385b9f4aa2a7136
jml <jiaominlong@outlook.com>
2016-09-23 14:57:55 +0800

    测试微信页面

--- wei_index.php
+++ wei_index.php
@@ -6,4 +6,25 @@
  * Time: 14:44
  */
 
-echo "Hello Weixin";
+
+// * 1.将timestamp，nonce,token按字段徐排序
+    $timestamp  =   $_GET['timestamp'];
+    $nonce      =   $_GET['nonce'];
+    $token      =   'weixin';
+    $signature  =   $_GET['signature'];
+
+    $arr        =   array($token,$timestamp,$nonce);
+    sort($arr, SORT_STRING);
+
+// * 2.将排序后的三个参数拼接成字符串后用sha1加密
+
+    $tmpStr     =   implode($arr);
+    $tmpStr     =   sha1($tmpStr);
+
+// * 3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
+
+    if ($signature == $tmpStr){
+        echo $_GET['echostr'];
+        exit();
+    }
+
